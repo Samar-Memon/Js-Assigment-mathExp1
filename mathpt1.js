26a37242ccb691a47c7adcb5bdbd607097da3caf
Samar-Memon <samar56786raza@gmail.com>
2024-03-09 22:41:53 +0500

    Js-Assignment-mathRxp1

---
+++ mathpt1.js
@@ -0,0 +1,23 @@
+var num;
+alert('num = ' + num);
+document.write('num = ' + num + '<br>' + '<br>');
+
+num = 4;
+alert('num After updated = ' + num);
+document.write('num After updated =  ' + num + '<br>' + '<br>');
+
+num++;
+alert('num After Increment = ' + num);
+document.write('num After Increment =  ' + num + '<br>' + '<br>');
+
+num = num + 6;
+alert('num After Addition = ' + num);
+document.write('num After Addtion =  ' + num + '<br>' + '<br>');
+
+num--;
+alert('num After Decrement = ' + num);
+document.write('num After Decrement =  ' + num + '<br>' + '<br>');
+
+num = num%2;
+alert('num After Remender = ' + num);
+document.write('num After Remeder =  ' + num + '<br>' + '<br>');
